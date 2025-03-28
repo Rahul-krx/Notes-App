a5c894f81e25f75af31913ab8f6922c67a155d42
Rahul-krx <krahul86946@gmail.com>
2025-03-28 18:44:19 +0530

    file structure created

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const port = 5000 || process.env.PORT;
+
+app.use(express.urlencoded ({extended: true}));
+app.use(express.json());
+
+// static files
+
+app.use(express.static('public'));
+
+// Templating engine
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+// routes
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, () =>{
+    console.log(`Server running on port ${port}`);
+})
+
